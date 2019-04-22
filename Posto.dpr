30f2385737eb6fc9b04f4b8bb0a0415e99c90955
Erica <ericaborges97@hotmail.com>
2019-04-22 20:55:26 -0300

    segunda aula

--- Posto.dpr
+++ Posto.dpr
@@ -3,12 +3,14 @@ program Posto;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  U_Posto in 'U_Posto.pas' {Form4};
+  U_Posto in 'U_Posto.pas' {Form4},
+  U_Login in 'U_Login.pas' {F_Login};
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.CreateForm(TF_Login, F_Login);
   Application.CreateForm(TForm4, Form4);
   Application.Run;
 end.
